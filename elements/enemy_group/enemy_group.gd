506548b24a2bc8a8df59102d52e306697ab30c27
kqzold <kirill.kyrve@ivkhk.ee>
2025-09-10 15:09:02 +0300

    fixed collision and timer issue

--- elements/enemy_group/enemy_group.gd
+++ elements/enemy_group/enemy_group.gd
@@ -6,7 +6,12 @@ const ROW_STEP = 10.0
 
 var direction := Vector2.RIGHT
 var speed := 20.0
+var can_change = true
 
+func _ready():
+	block_timer.timeout.connect(_on_block_timer_timeout)
+	
+	
 func _process(delta: float) -> void:
 	global_position += direction * speed * delta
 
@@ -14,8 +19,14 @@ func _process(delta: float) -> void:
 func change_direction():
 	print("prepare direction change")
 	if block_timer.time_left > 0:
-		return
+		if not can_change:
+			return
 	print("Direction changed")
 	direction = Vector2.LEFT if direction == Vector2.RIGHT else Vector2.RIGHT
 	global_position.y += ROW_STEP
+	
+	can_change = false
 	block_timer.start()
+	
+func _on_block_timer_timeout():
+	can_change = true
