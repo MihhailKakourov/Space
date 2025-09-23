28c0e95495f04a21a65182b41390c93234f151cb
lRaxSonl <malenkijh@gmail.com>
2025-09-23 15:01:00 +0300

    Fixing enemies movement algorithm

--- elements/enemies/scripts/BasicEnemyScript.gd
+++ elements/enemies/scripts/BasicEnemyScript.gd
@@ -2,15 +2,17 @@ extends CharacterBody2D
 class_name BasicEnemy
 
 @export var speed = 80
-@export var stop_distance: float = 25.0  #на каком расстоянии остановиться
+@export var stop_distance: float = 30.0  #на каком расстоянии остановиться
 @onready var sprite = $AnimatedSprite2D
-@onready var shape = $CollisionShape2D
+@onready var shape_left: CollisionShape2D = $CollisionShape2DLeft
+@onready var shape_right: CollisionShape2D = $CollisionShape2D2Right
 var player: Node2D
 
 #TODO: Пофиксить коллизию после смены направления взгляда врага
 
 func _ready() -> void:
 	player = get_tree().root.get_node("game/Player")
+	print(player)
 
 
 func _physics_process(delta: float) -> void:
@@ -20,21 +22,31 @@ func _physics_process(delta: float) -> void:
 func move_to_player() -> void:
 	if is_instance_valid(player):
 		var distance = global_position.distance_to(player.global_position)
+		print("Distance: " + str(distance) + " | Stop Distance: " + str(stop_distance))
 		
 		if distance > stop_distance:
-			var dir: Vector2 = (player.global_position - global_position).normalized()
-			velocity = dir * speed
+			var dir: Vector2 = get_direction_to_player()
+			if dir:
+				velocity = dir * speed
+			else:
+				velocity = Vector2.ZERO
 		else:
 			velocity = Vector2.ZERO
 			
-		movment_animation()
+		movement_animation()
 		move_and_slide()
 		
-func movment_animation() -> void:
+func movement_animation() -> void:
 	if velocity.length() > 0.1:
-		#Смотрит в сторону движения
 		if abs(velocity.x) > 0.1:
-			body.scale.x = sign(velocity.x) #-1 if velocity.x < 0 else 1
+			# flip sprite и переключаем коллизию
+			sprite.flip_h = velocity.x < 0
+			if velocity.x < 0:
+				shape_left.disabled = true
+				shape_right.disabled = false
+			else:
+				shape_left.disabled = false
+				shape_right.disabled = true
 		sprite.animation = "movment"
 		sprite.play()
 	else:
