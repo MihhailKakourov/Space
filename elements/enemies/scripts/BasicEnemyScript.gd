9a3aa9d56841b3bbdc7b3b01a1fe82030f414b7a
lRaxSonl <malenkijh@gmail.com>
2025-09-24 21:02:38 +0300

    enemies attack in progress

--- elements/enemies/scripts/BasicEnemyScript.gd
+++ elements/enemies/scripts/BasicEnemyScript.gd
@@ -4,15 +4,19 @@ class_name BasicEnemy
 @export var speed = 80
 @export var stop_distance: float = 26.0  #на каком расстоянии остановиться
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var attackA: AnimationPlayer = $AnimationPlayer
 @onready var shape_left: CollisionShape2D = $CollisionShape2DLeft
 @onready var shape_right: CollisionShape2D = $CollisionShape2DRight
 var player: Node2D
+var attacking := false
+
 
 #TODO: Пофиксить коллизию после смены направления взгляда врага
 
 func _ready() -> void:
 	player = get_tree().root.get_node("game/Player")
-	print(player)
+	attackA.animation_finished.connect(_on_attack_finished)
+	#print(player)
 
 
 func _physics_process(delta: float) -> void:
@@ -32,6 +36,8 @@ func move_to_player() -> void:
 				velocity = Vector2.ZERO
 		else:
 			velocity = Vector2.ZERO
+			if not attacking:
+				attack()
 			
 		movement_animation()
 		move_and_slide()
@@ -53,8 +59,15 @@ func movement_animation() -> void:
 	else:
 		sprite.animation = "idle"
 		sprite.play()
-
+		
+func attack() -> void:
+	attackA.play("attack")
+	
 func get_direction_to_player() -> Vector2:
 	if is_instance_valid(player):
 		return (player.global_position - global_position).normalized()
 	return Vector2.ZERO
+	
+func _on_attack_finished(name: String) -> void:
+	if name == "attack":
+		attacking = false
