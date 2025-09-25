51033c64d4ca5bacd4428951fecb2b0caa5d9817
Daniel <danielkisseljov96@gmail.com>
2025-09-25 09:33:45 +0300

    fixed pause

--- elements/game_over/game_over.gd
+++ elements/game_over/game_over.gd
@@ -1,7 +1,18 @@
 extends CanvasLayer
 
-
+func _ready() -> void:
+	get_tree().paused = true
 
 func _on_restart_button_pressed() -> void:
+	get_tree().paused = false
 	print("pressed")
+	_restart_stats()
 	get_tree().reload_current_scene()
+
+
+func _restart_stats() -> void:
+	for enemy in get_tree().get_nodes_in_group("Enemies"):
+		if is_instance_valid(enemy):
+			enemy.queue_free()
+	Globals.hp = 100
+	Globals.coin = 0
